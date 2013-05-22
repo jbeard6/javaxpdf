4e81750c58859706c0981beded05c8fc9e190793
Joseph Beard <beardj@oclc.org>
2013-05-22 13:57:04 -0400

    Updated Java project to depend on Java 1.6 and built a small test driver program.

---
+++ java/src/main/java/net/josephbeard/xpdf/ExtractText.java
@@ -0,0 +1,28 @@
+package net.josephbeard.xpdf;
+
+import java.io.File;
+
+public class ExtractText {
+
+	public static void main(String[] args) {
+		File libraryFile = new File(args[0]);
+		System.load(libraryFile.getAbsolutePath());
+		// System.loadLibrary("xpdf-jni");
+
+		for (int i = 1; i < args.length; i++) {
+			String fileName = args[i];
+			File file = new File(fileName);
+			if (!file.exists()) {
+				System.err.println(fileName + " does not exist!");
+				continue;
+			}
+
+			PDFDocument document = PDFDocument.createInstance(file);
+
+			for (PDFPage page : document.getPages()) {
+				System.out.println(page.getText());
+			}
+		}
+	}
+
+}
