f70dc9a96b031182758217ba1e28137d9ea868be
Joseph Beard <beardj@oclc.org>
2013-05-28 10:20:59 -0400

    Added Javadoc for the majority of the methods.

--- src/main/java/com/foolabs/xpdf/GlobalParameters.java
+++ src/main/java/com/foolabs/xpdf/GlobalParameters.java
@@ -1,121 +1,244 @@
+
 package com.foolabs.xpdf;
 
 import java.nio.charset.Charset;
 
 /**
- * Java abstraction for the Xpdf GlobalParams class. Warning: this is a
- * singleton instance across the ENTIRE JVM! Options set here will affect ALL
- * users of Xpdf in the JVM.
+ * Java abstraction for the Xpdf GlobalParams class. Warning: this is a singleton instance across the ENTIRE JVM!
+ * Options set here will affect ALL users of Xpdf in the JVM.
  */
 public class GlobalParameters {
 
-	private static GlobalParameters INSTANCE;
+    /** The singleton instance. */
+    private static GlobalParameters INSTANCE;
 
-	public static GlobalParameters getInstance() {
-		if (INSTANCE == null) {
-			long _handle = _getInstance();
-			INSTANCE = new GlobalParameters(_handle);
-			
-			// Initialize with UTF-8 encoding
-			INSTANCE._setTextEncoding("UTF-8");
-		}
+    /**
+     * Return the singleton {@link GlobalParameters} instance.
+     * 
+     * @return the singleton instance
+     */
+    public static GlobalParameters getInstance() {
+        if (INSTANCE == null) {
+            long _handle = _getInstance();
+            INSTANCE = new GlobalParameters(_handle);
 
-		return INSTANCE;
-	}
+            // Initialize with UTF-8 encoding
+            INSTANCE._setTextEncoding("UTF-8");
+        }
 
-	private static native long _getInstance();
+        return INSTANCE;
+    }
 
-	private GlobalParameters(long _handle) {
-		this._handle = _handle;
-	}
+    /**
+     * Obtains the handle to the native {@code GlobalParams} object.
+     * 
+     * @return the native handle
+     */
+    private static native long _getInstance();
 
-	private final long _handle;
+    private GlobalParameters(long _handle) {
+        this._handle = _handle;
+    }
 
-	public Charset getCharset() {
-		String encoding = _getTextEncoding();
-		return Charset.forName(encoding);
-	}
+    /** The native handle. */
+    private final long _handle;
 
-	public void setCharset(Charset charset) {
-		if (charset == null) {
-			throw new NullPointerException();
-		}
-		_setTextEncoding(charset.name());
-	}
+    /**
+     * Returns the {@link Charset} that will be used for reading {@link PDFDocument} text.
+     * 
+     * @return the character set
+     */
+    public Charset getCharset() {
+        String encoding = _getTextEncoding();
+        return Charset.forName(encoding);
+    }
 
-	public String getTextEncoding() {
-		return _getTextEncoding();
-	}
+    /**
+     * Sets the {@link Charset} that will be used for reading {@link PDFDocument} text.
+     * 
+     * @param charset the character set
+     * @throws NullPointerException if the {@link Charset} is <code>null</code>
+     */
+    public void setCharset(Charset charset) {
+        if (charset == null) {
+            throw new NullPointerException();
+        }
+        _setTextEncoding(charset.name());
+    }
 
-	private native String _getTextEncoding();
+    /**
+     * Returns the name of the {@link Charset} that will be used for reading {@link PDFDocument} text.
+     * 
+     * @return the encoding scheme
+     */
+    public String getTextEncoding() {
+        return _getTextEncoding();
+    }
 
-	public void setTextEncoding(String encoding) {
-		_setTextEncoding(encoding);
-	}
+    /**
+     * Returns the encoding scheme from the native object.
+     * 
+     * @return the encoding scheme
+     */
+    private native String _getTextEncoding();
 
-	private native void _setTextEncoding(String encoding);
+    public void setTextEncoding(String encoding) {
+        _setTextEncoding(encoding);
+    }
 
-	public static enum EndOfLine {
-		UNIX("unix"), DOS("dos"), MAC("mac");
+    /**
+     * Sets the encoding scheme on the native object.
+     * 
+     * @param encoding the encoding scheme
+     */
+    private native void _setTextEncoding(String encoding);
 
-		private EndOfLine(String value) {
-			this.value = value;
-		}
+    /**
+     * The end-of-line style for emitted output.
+     */
+    public static enum EndOfLine {
+        /** The UNIX EOL style: <code>\n</code>. */
+        UNIX("unix"),
 
-		private final String value;
+        /** The DOS EOL style: <code>\r\n</code>. */
+        DOS("dos"),
 
-		String getValue() {
-			return value;
-		}
-	}
+        /** The Mac EOL style: <code>\r</code>. */
+        MAC("mac");
 
-	public EndOfLine getEndOfLine() {
-		String eol = _getTextEOL();
+        /**
+         * Construct a new {@link EndOfLine}.
+         * 
+         * @param value the native value
+         */
+        private EndOfLine(String value) {
+            this.value = value;
+        }
 
-		for (EndOfLine e : EndOfLine.values()) {
-			if (e.getValue() == eol) {
-				return e;
-			}
-		}
+        /** The native value. */
+        private final String value;
 
-		return null;
-	}
+        /**
+         * Returns the native value.
+         * 
+         * @return the native value
+         */
+        String getValue() {
+            return value;
+        }
+    }
 
-	private native String _getTextEOL();
+    /**
+     * Returns the {@link EndOfLine} style used for emitted text.
+     * 
+     * @return the end of line style
+     */
+    public EndOfLine getEndOfLine() {
+        String eol = _getTextEOL();
 
-	public void setEndOfLine(EndOfLine eol) {
-		if (eol == null) {
-			throw new NullPointerException();
-		}
-		_setTextEOL(eol.getValue());
-	}
+        for (EndOfLine e : EndOfLine.values()) {
+            if (e.getValue() == eol) {
+                return e;
+            }
+        }
 
-	private native void _setTextEOL(String eol);
+        return null;
+    }
 
-	public boolean isBreakBetweenPages() {
-		return _getTextPageBreaks();
-	}
+    /**
+     * Returns the native value for the {@link EndOfLine} from the native object.
+     * 
+     * @return the native EOL style
+     */
+    private native String _getTextEOL();
 
-	private native boolean _getTextPageBreaks();
+    /**
+     * Set the {@link EndOfLine} style for {@link PDFDocument} text.
+     * 
+     * @param eol the EOL style
+     */
+    public void setEndOfLine(EndOfLine eol) {
+        if (eol == null) {
+            throw new NullPointerException();
+        }
+        _setTextEOL(eol.getValue());
+    }
 
-	public void setBreakBetweenPages(boolean breakBetweenPages) {
-		_setTextPageBreaks(breakBetweenPages);
-	}
+    /**
+     * Sets the native EOL style.
+     * 
+     * @param eol the native {@link EndOfLine} value
+     */
+    private native void _setTextEOL(String eol);
 
-	private native void _setTextPageBreaks(boolean textPageBreaks);
+    /**
+     * Returns <code>true</code> if a form feed character will be written between {@link PDFPage#getText() page text}.
+     * 
+     * @return <code>true</code> if page content is to be split
+     */
+    public boolean isBreakBetweenPages() {
+        return _getTextPageBreaks();
+    }
 
-	public boolean isErrorOutputSuppressed() {
-		return _getErrQuiet();
-	}
+    /**
+     * Returns <code>true</code> if page content is to be split according to the native object.
+     * 
+     * @return <code>true</code> if page content is to be split
+     */
+    private native boolean _getTextPageBreaks();
 
-	private native boolean _getErrQuiet();
+    /**
+     * Sets if a form feed character will be written between {@link PDFPage#getText() page text}.
+     * 
+     * @param breakBetweenPages <code>true</code> if page content is to be split
+     */
+    public void setBreakBetweenPages(boolean breakBetweenPages) {
+        _setTextPageBreaks(breakBetweenPages);
+    }
 
-	public void setErrorOutputSuppressed(boolean errorOutputSuppressed) {
-		_setErrQuiet(errorOutputSuppressed);
-	}
+    /**
+     * Sets if page content is to be split on the native object.
+     * 
+     * @param textPageBreaks <code>true</code> if page content is to be split
+     */
+    private native void _setTextPageBreaks(boolean textPageBreaks);
 
-	private native void _setErrQuiet(boolean errQuiet);
+    /**
+     * Returns <code>true</code> if error output is suppressed.
+     * 
+     * @return <code>true</code> if error output is suppressed
+     */
+    public boolean isErrorOutputSuppressed() {
+        return _getErrQuiet();
+    }
 
-	protected native void finalize();
+    /**
+     * Returns <code>true</code> if error output is suppressed according to the native object.
+     * 
+     * @return <code>true</code> if error output is suppressed
+     */
+    private native boolean _getErrQuiet();
+
+    /**
+     * Set to <code>true</code> to suppress error output.
+     * 
+     * @param errorOutputSuppressed <code>true</code> if error output should be suppressed
+     */
+    public void setErrorOutputSuppressed(boolean errorOutputSuppressed) {
+        _setErrQuiet(errorOutputSuppressed);
+    }
+
+    /**
+     * Sets whether error output should be suppressed on the native object.
+     * 
+     * @param errQuiet <code>true</code> if error output should be suppressed
+     */
+    private native void _setErrQuiet(boolean errQuiet);
+
+    /**
+     * Delete the native {@code GlobalParams} object.
+     */
+    @Override
+    protected native void finalize();
 
 }
--- src/main/java/com/foolabs/xpdf/PDFDocument.java
+++ src/main/java/com/foolabs/xpdf/PDFDocument.java
@@ -1,83 +1,135 @@
+
 package com.foolabs.xpdf;
 
 import static java.util.logging.Level.FINER;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Logger;
 
+/**
+ * Abstraction for an Xpdf document. Due to limitations of the current version of Xpdf, all {@link PDFDocument}
+ * instances must be backed by a {@link File}.
+ */
 public final class PDFDocument {
 
-	private static final Logger LOGGER = Logger.getLogger(PDFDocument.class
-			.getName());
+    /** The class-level {@link Logger}. */
+    private static final Logger LOGGER = Logger.getLogger(PDFDocument.class.getName());
 
-	public static PDFDocument createInstance(File file)
-			throws InstantiationException {
-		String fileName = file.getAbsolutePath();
-		long _handle = _createInstance(fileName);
+    /**
+     * Create a {@link PDFDocument} instance that represents the specified PDF {@link File}.
+     * 
+     * @param file the PDF file
+     * @return the new instance
+     * @throws InstantiationException if Xpdf does not correctly initialize the document
+     */
+    public static PDFDocument createInstance(File file) throws InstantiationException {
+        String fileName = file.getAbsolutePath();
+        long _handle = _createInstance(fileName);
 
-		if (_handle >= 0) {
-			LOGGER.log(FINER, "Created native PDFDoc instance @{0}", _handle);
-			return new PDFDocument(_handle);
-		} else {
-			throw new InstantiationException("XPDF Blows");
-		}
-	}
+        if (_handle >= 0) {
+            LOGGER.log(FINER, "Created native PDFDoc instance @{0}", _handle);
+            return new PDFDocument(_handle);
+        } else {
+            throw new InstantiationException("XPDF Blows");
+        }
+    }
 
-	private static native long _createInstance(String fileName);
+    /**
+     * Create the native objects.
+     * 
+     * @param fileName the absolute path to the {@link File}
+     * @return the {@link #_handle}
+     */
+    private static native long _createInstance(String fileName);
 
-	private PDFDocument(long _handle) {
-		this._handle = _handle;
-	}
+    /**
+     * Construct a new {@link PDFDocument} backed by the PDFDoc pointed to by the {@code _handle}.
+     * 
+     * @param _handle the {@link #_handle}
+     */
+    private PDFDocument(long _handle) {
+        this._handle = _handle;
+    }
 
-	private final long _handle;
+    /** Pointer to the native object. */
+    private final long _handle;
 
-	/**
-	 * Returns the number of pages in this {@link PDFDocument}.
-	 * 
-	 * @return the page count
-	 */
-	public int getPageCount() {
-		return _getPageCount();
-	}
+    /**
+     * Returns the number of pages in this {@link PDFDocument}.
+     * 
+     * @return the page count
+     */
+    public int getPageCount() {
+        return _getPageCount();
+    }
 
-	private native int _getPageCount();
+    /**
+     * Returns the page count from the native object.
+     * 
+     * @return the page count
+     */
+    private native int _getPageCount();
 
-	/**
-	 * Returns <code>true</code> if this {@link PDFDocument} is encrypted.
-	 * 
-	 * @return <code>true</code> if this document is encrypted
-	 */
-	public boolean isEncrypted() {
-		return _isEncrypted();
-	}
+    /**
+     * Returns <code>true</code> if this {@link PDFDocument} is encrypted.
+     * 
+     * @return <code>true</code> if this document is encrypted
+     */
+    public boolean isEncrypted() {
+        return _isEncrypted();
+    }
 
-	private native boolean _isEncrypted();
+    /**
+     * Returns the encryption status from the native object.
+     * 
+     * @return <code>true</code> if the document is encrypted
+     */
+    private native boolean _isEncrypted();
 
-	public PDFPage getPage(int pageNumber) {
-		if (pageNumber <= 0 || pageNumber > _getPageCount()) {
-			throw new IllegalArgumentException();
-		}
-		return _getPage(pageNumber);
-	}
+    /**
+     * Returns the {@link PDFPage} that represents the specified page.
+     * 
+     * @param pageNumber the (one-based) page number
+     * @return the page object
+     */
+    public PDFPage getPage(int pageNumber) {
+        if (pageNumber <= 0 || pageNumber > _getPageCount()) {
+            throw new IllegalArgumentException();
+        }
+        return _getPage(pageNumber);
+    }
 
-	private native PDFPage _getPage(int pageNumber);
+    /**
+     * Returns the {@link PDFPage} from the native object.
+     * 
+     * @param pageNumber the page number (one-based)
+     * @return the page object
+     */
+    private native PDFPage _getPage(int pageNumber);
 
-	public PDFPage[] getPages() {
-		int pageCount = _getPageCount();
-		LOGGER.log(FINER, "Document has {0} pages.", pageCount);
+    /**
+     * Returns the {@link PDFPage pages} of this {@link PDFDocument}.
+     * 
+     * @return the pages
+     */
+    public PDFPage[] getPages() {
+        int pageCount = _getPageCount();
+        LOGGER.log(FINER, "Document has {0} pages.", pageCount);
 
-		PDFPage[] pages = new PDFPage[pageCount];
+        PDFPage[] pages = new PDFPage[pageCount];
 
-		for (int i = 0; i < pageCount; i++) {
-			pages[i] = _getPage(i + 1);
-			LOGGER.log(FINER, "Successfully obtained Page {0}.", i);
-		}
+        for (int i = 0; i < pageCount; i++) {
+            pages[i] = _getPage(i + 1);
+            LOGGER.log(FINER, "Successfully obtained Page {0}.", i);
+        }
 
-		return pages;
-	}
+        return pages;
+    }
 
-	protected native void finalize();
+    /**
+     * Releases the native objects.
+     */
+    @Override
+    protected native void finalize();
 
 }
